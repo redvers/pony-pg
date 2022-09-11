70726351e196d56b084c0cc23454484ec17c37ae
Redvers Davies <red@infect.me>
2022-09-11 12:50:24 -0400

    Remove circleci

--- .ci-scripts/env.bash
+++
@@ -1,8 +0,0 @@
-REPO_OWNER="redvers"
-REPO_NAME="pony-pg"
-GITHUB_USER="redvers"
-
-# Who we are for git
-git config --global user.email "red@infect.me"
-git config --global user.name "Redvers Davies"
-git config --global push.default simple
